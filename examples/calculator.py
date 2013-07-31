f2f309713eda966e13641bd1f403420ebb6fa64c
Dan Cuellar <penguinho@aol.com>
2013-07-30 19:41:52 -0700

    working calculator example

--- examples/calculator.py
+++ examples/calculator.py
@@ -2,9 +2,16 @@ from selenium import webdriver
 
 driver = webdriver.Remote( command_executor='http://localhost:8080/wd/hub', desired_capabilities={'platform':'Mac'})
 
-driver.get("TextEdit")
-source = driver.page_source
-print source
+driver.get("Calculator")
+
+e2 = driver.find_element_by_name('2')
+eplus = driver.find_element_by_name('+')
+eequals = driver.find_element_by_name('=')
+
+e2.click()
+eplus.click()
+e2.click()
+eequals.click()
 
 # quit the webdriver instance
 print 'Quitting webdriver\n'
