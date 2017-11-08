7470b4b4cd9fcc63d03d2ab7901c4448201b1ee9
usher <usher@ushers-Mac.local>
2017-11-08 12:47:49 +0800

    fix post_elements

--- AppiumForMac/Server/Handlers/AfMHandlers.m
+++ AppiumForMac/Server/Handlers/AfMHandlers.m
@@ -543,7 +543,7 @@
                     [elements addObject:[NSDictionary dictionaryWithObject:myKey forKey:@"ELEMENT"]];
                 }
                 
-                return [AppiumMacHTTPJSONResponse responseWithJson:nil status:kAfMStatusCodeNoSuchElement session:session.sessionId];
+                return [AppiumMacHTTPJSONResponse responseWithJson:elements status:kAfMStatusCodeSuccess session:session.sessionId];
             }
         }
         
