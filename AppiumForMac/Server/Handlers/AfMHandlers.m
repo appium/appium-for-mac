521cd799fa01b4f0f8cc494c559e5e9b0ae883c4
David Mokos <david.mokos@avast.com>
2017-12-14 13:59:06 +0100

    Removes redundant else statement

--- AppiumForMac/Server/Handlers/AfMHandlers.m
+++ AppiumForMac/Server/Handlers/AfMHandlers.m
@@ -544,12 +544,9 @@
                 }
                 
                 return [AppiumMacHTTPJSONResponse responseWithJson:elements status:kAfMStatusCodeSuccess session:session.sessionId];
-                
-            } else {
-                return [AppiumMacHTTPJSONResponse responseWithJson:nil status:kAfMStatusCodeNoSuchElement session:session.sessionId];
             }
+            return [AppiumMacHTTPJSONResponse responseWithJson:nil status:kAfMStatusCodeNoSuchElement session:session.sessionId];
         }
-        
         return [AppiumMacHTTPJSONResponse responseWithJsonError:*statusCode session:session.sessionId];
     }];
 }
